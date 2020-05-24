7c62b5dfef51d826fdb711c48cd9dd5597a44a3f
Neur1n <Jihang.Li@outlook.com>
2020-05-24 16:02:28 +0800

    feat(deploy): add deployment script for Windows terminal and powershell core

---
+++ deploy/win/pwsh.bat
@@ -0,0 +1,3 @@
+@echo off
+
+MKLINK %USERPROFILE%\Documents\Powershell\Microsoft.Powershell_profile.ps1 %CD%\..\..\pwsh\Microsoft.PowerShell_profile.ps1
---
+++ deploy/win/wt.bat
@@ -0,0 +1,3 @@
+@echo off
+
+MKLINK %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %CD%\..\..\wt\settings.json
