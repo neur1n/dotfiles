a6a3c96badbc9284576ed6f3afb95aecae28e41e
Neur1n <Jihang.Li@outlook.com>
2021-05-24 03:23:33 -0700

    fix(deploy): pwsh deployment script

--- deploy/linux/pwsh.bash
+++ deploy/linux/pwsh.bash
@@ -5,6 +5,3 @@ if [[ ! -d "~/.config/powershell" ]]; then
 fi
 
 ln -ns `ls -d1 $PWD/../../pwsh/*` ~/.config/powershell/
-
-mkdir -p ~/.config/nvim/recovery/backup
-mkdir -p ~/.config/nvim/recovery/session
