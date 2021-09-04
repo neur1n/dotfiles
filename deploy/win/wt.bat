cec5993f4ba0936302cf1eee4b949ef4eaa6608c
Neur1n <Jihang.Li@outlook.com>
2021-09-04 12:22:25 +0800

    chore(wt): use windows terminal preview instead of stable

--- deploy/win/wt.bat
+++ deploy/win/wt.bat
@@ -1,3 +1,3 @@
 @echo off
 
-MKLINK %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json %CD%\..\..\wt\settings.json
+MKLINK %LOCALAPPDATA%\Packages\Microsoft.WindowsTerminalPreview_8wekyb3d8bbwe\LocalState\settings.json %CD%\..\..\wt\settings.json
