818a0c91bd3cdd7cb49275cb2382a9bcf4e7f7e7
Neur1n <Jihang.Li@outlook.com>
2022-05-27 10:21:36 +0800

    refactor(deploy): make neovim.cmd glob files and dirs

--- deploy/win/neovim.cmd
+++ deploy/win/neovim.cmd
@@ -1,19 +1,14 @@
 @echo off
 
-MKLINK /J %LOCALAPPDATA%\nvim\after %CD%\..\..\neovim\after
-MKLINK /J %LOCALAPPDATA%\nvim\autoload %CD%\..\..\neovim\autoload
-MKLINK /J %LOCALAPPDATA%\nvim\colors %CD%\..\..\neovim\colors
-MKLINK /J %LOCALAPPDATA%\nvim\configs %CD%\..\..\neovim\configs
-MKLINK /J %LOCALAPPDATA%\nvim\indent %CD%\..\..\neovim\indent
-MKLINK /J %LOCALAPPDATA%\nvim\lua %CD%\..\..\neovim\lua
-MKLINK /J %LOCALAPPDATA%\nvim\plugged %CD%\..\..\neovim\plugged
-MKLINK /J %LOCALAPPDATA%\nvim\spell %CD%\..\..\neovim\spell
-MKLINK /J %LOCALAPPDATA%\nvim\syntax %CD%\..\..\neovim\syntax
-MKLINK /J %LOCALAPPDATA%\nvim\templates %CD%\..\..\neovim\templates
+FOR /F %%d IN ('DIR /B /A:D %CD%\..\..\neovim\*') DO (
+  MKLINK /J %LOCALAPPDATA%\nvim\%%d %CD%\..\..\neovim\%%d
+)
 
-MKLINK %LOCALAPPDATA%\nvim\coc-settings.json %CD%\..\..\neovim\coc-settings.json
-MKLINK %LOCALAPPDATA%\nvim\ginit.vim %CD%\..\..\neovim\ginit.vim
-MKLINK %LOCALAPPDATA%\nvim\init.vim %CD%\..\..\neovim\init.vim
+FOR /F %%f IN ('DIR /B /A-D %CD%\..\..\neovim\*') DO (
+  MKLINK %LOCALAPPDATA%\nvim\%%f %CD%\..\..\neovim\%%f
+)
 
 MKDIR %LOCALAPPDATA%\nvim\recovery\backup
 MKDIR %LOCALAPPDATA%\nvim\recovery\session
+
+START %LOCALAPPDATA%\nvim
