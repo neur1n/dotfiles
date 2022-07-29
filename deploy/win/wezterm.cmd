51e98ccccf28ebb360e192110054c907d18b938f
Neur1n <Jihang.Li@outlook.com>
2022-07-29 18:39:40 +0800

    feat(wezterm): added configs for wezterm

--- deploy/win/wezterm.cmd
+++ deploy/win/wezterm.cmd
@@ -3,6 +3,7 @@
 SET src=%~dp0.
 SET dst=%USERPROFILE%
 
+MKLINK /J %dst%\.wezterm %src%\..\..\wezterm\.wezterm
 MKLINK %dst%\.wezterm.lua %src%\..\..\wezterm\.wezterm.lua
 
 START %dst%
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,4 +1,5 @@
-local wezterm = require "wezterm"
+local Colorschemes = require("colorschemes")
+local Wezterm = require("wezterm")
 
 local fonts = {
 {["name"] = "Anonymice NF"        , ["size"] = 13},
@@ -8,17 +9,20 @@ local fonts = {
 {["name"] = "FiraCode NF"         , ["size"] = 11},
 {["name"] = "GohuFont NF"         , ["size"] = 12},
 {["name"] = "Input NF"            , ["size"] = 12},
+{["name"] = "JetBrainsMono NF"    , ["size"] = 11},
 {["name"] = "ProggyCleanTT NF"    , ["size"] = 16},
+{["name"] = "SauceCodePro NF"     , ["size"] = 11},
 {["name"] = "SpaceMono NF"        , ["size"] = 11},
 {["name"] = "VictorMono NF"       , ["size"] = 12},
 }
 local font = fonts[math.random(#fonts)]
 
 return {
+  color_scheme = Colorschemes.get(),
   default_cwd = ".",
   default_prog = {"nu", ""},
   enable_scroll_bar = true,
-  font = wezterm.font(font["name"]),
+  font = Wezterm.font(font["name"]),
   font_size = font["size"],
   initial_cols = 120,
   initial_rows = 30,
@@ -35,6 +39,8 @@ return {
   tab_bar_at_bottom = true,
   window_close_confirmation = "NeverPrompt",
   keys = {
-    {key = "l", mods = "CTRL|SHIFT", action = "ShowLauncher"},
+    {key = "l", mods = "CTRL|SHIFT", action = Wezterm.action.ShowLauncher},
+    {key = "_", mods = "CTRL|SHIFT", action = Wezterm.action.SplitPane {direction = "Down"}},
+    {key = "+", mods = "CTRL|SHIFT", action = Wezterm.action.SplitPane {direction = "Right"}},
   },
 }
