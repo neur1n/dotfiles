c834145ae8845be0f0d0d5295b23c33c99ac24ed
neur1n <jihangli@duck.com>
2024-08-23 10:17:24 +0800

    feat(neovim): use treesitter to get current function
    Note that this is buggy for some languages, e.g., cuda

--- neovim/lua/plugconf/noline/tag.lua
+++ neovim/lua/plugconf/noline/tag.lua
@@ -2,11 +2,37 @@ local M = {}
 
 local Component = require("noline.utility.component")
 
+local function treesitter_current_function()
+  local node = vim.treesitter.get_node()
+  if not node then
+    return ""
+  end
+
+  local expr = node
+  while expr do
+    if expr:type() == "function_declaration" or expr:type() == "function_definition" then
+      break
+    end
+
+    expr = expr:parent()
+  end
+
+  if not expr then
+    return ""
+  end
+
+  expr = vim.treesitter.get_node_text(expr:child(1), 0)
+
+  return string.gsub(expr, "%b()", "")
+end
+
 function M.get(symbol, l_decor, r_decor)
   local expr = ""
 
   if vim.b.coc_current_function ~= nil then
     expr = vim.b.coc_current_function
+  else
+    expr = treesitter_current_function()
   end
 
   if expr ~= "" and type(symbol) == "string" then
