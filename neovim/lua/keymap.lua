95409285b05cb63eb9acbc6e6d9889f396ce28b2
neur1n <jihangli@duck.com>
2024-11-18 10:54:22 +0800

    several changes
    1. feat(neovim): map j/k to gj/gk
    2. chore(neovim): add encoding to shebang snippet

--- neovim/lua/keymap.lua
+++ neovim/lua/keymap.lua
@@ -6,6 +6,8 @@ function M.setup()
   vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {noremap = true})
 
   vim.keymap.set("n", "G", "Gzz", {noremap = true})
+  vim.keymap.set({"n", "v"}, "gj", "j", {noremap = true})
+  vim.keymap.set({"n", "v"}, "gk", "k", {noremap = true})
   vim.keymap.set({"n", "v"}, "j", "gj", {noremap = true})
   vim.keymap.set({"n", "v"}, "k", "gk", {noremap = true})
 
