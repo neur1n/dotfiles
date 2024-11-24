e0a67ce163935bd67a5966d1b14cc27d6d2e8914
neur1n <jihangli@duck.com>
2024-11-24 15:53:50 +0800

    feat(neovim): add rainbow highlighting to indent-blank

--- neovim/lua/plugconf/indent-blankline.lua
+++ neovim/lua/plugconf/indent-blankline.lua
@@ -1,9 +1,32 @@
 local M = {}
 
+
 function M.setup()
+  local rainbow = {
+    "IBLRainbowRed",
+    "IBLRainbowOrange",
+    "IBLRainbowYellow",
+    "IBLRainbowGreen",
+    "IBLRainbowCyan",
+    "IBLRainbowBlue",
+    "IBLRainbowPurple",
+  }
+
+  local hooks = require("ibl.hooks")
+
+  hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+    vim.api.nvim_set_hl(0, "IBLRainbowRed", {fg = "#E06C75"})
+    vim.api.nvim_set_hl(0, "IBLRainbowOrange", {fg = "#D19A66"})
+    vim.api.nvim_set_hl(0, "IBLRainbowYellow", {fg = "#E5C07B"})
+    vim.api.nvim_set_hl(0, "IBLRainbowGreen", {fg = "#98C379"})
+    vim.api.nvim_set_hl(0, "IBLRainbowCyan", {fg = "#56B6C2"})
+    vim.api.nvim_set_hl(0, "IBLRainbowBlue", {fg = "#61AFEF"})
+    vim.api.nvim_set_hl(0, "IBLRainbowPurple", {fg = "#C678DD"})
+  end)
+
   require("ibl").setup({
     enabled = false,
-    indent = {highlight = "Function"},
+    indent = {highlight = rainbow},
     whitespace = {highlight = "Normal"},
   })
 end
