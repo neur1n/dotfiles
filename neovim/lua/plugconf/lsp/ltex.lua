d75d0a30fc12099a836a0e54280adc66171b7cf5
neur1n <jihangli@duck.com>
2024-12-18 13:04:39 +0800

    feat(neovim): make ltex read local filter list

--- neovim/lua/plugconf/lsp/ltex.lua
+++ neovim/lua/plugconf/lsp/ltex.lua
@@ -1,9 +1,30 @@
 local M = {}
 
+local function local_dict()
+  local cwd = vim.fn.getcwd()
+
+  local dict = io.open(cwd .. "/ltex.dict", "r")
+  if dict == nil then
+    return nil
+  end
+
+  local words = {}
+  for line in dict:lines() do
+    table.insert(words, line)
+  end
+
+  dict:close()
+
+  return words
+end
+
 function M.setup(handlers)
   require("lspconfig").ltex.setup({
     settings = {
-      ltex = {language = "en-US"},
+      ltex = {
+        dictionary = {["en-US"] = local_dict()},
+        language = "en-US",
+      },
     },
     handlers = handlers,
   })
