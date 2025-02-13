c9a402448f0f129767d03584ecba44df30e7eb15
neur1n <jihangli@duck.com>
2025-02-13 14:33:43 +0800

    refactor(wezterm): reset keymap to ActivatePaneDirection

--- wezterm/.wezterm/n_keymap.lua
+++ wezterm/.wezterm/n_keymap.lua
@@ -3,10 +3,6 @@ local Wezterm = require("wezterm")
 local M = {}
 
 local map = {
-  {key = "UpArrow", mods = "ALT", action = Wezterm.action.ActivatePaneDirection("Up")},
-  {key = "DownArrow", mods = "ALT", action = Wezterm.action.ActivatePaneDirection("Down")},
-  {key = "LeftArrow", mods = "ALT", action = Wezterm.action.ActivatePaneDirection("Left")},
-  {key = "RightArrow", mods = "ALT", action = Wezterm.action.ActivatePaneDirection("Right")},
   {key = "_", mods = "ALT|SHIFT", action = Wezterm.action.SplitPane{direction = "Down"}},
   {key = "+", mods = "ALT|SHIFT", action = Wezterm.action.SplitPane{direction = "Right"}},
   {key = "f", mods = "ALT|SHIFT", action = Wezterm.action.EmitEvent("select-font")},
