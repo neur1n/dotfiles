269b2ed2458b2d76b2ff70dcbe1457750a567063
neur1n <Jihang.Li@outlook.com>
2025-08-10 15:31:13 +0800

    fix(neovim): add nvim-lspconfig back

--- neovim/lua/plugconf/lazy.lua
+++ neovim/lua/plugconf/lazy.lua
@@ -148,6 +148,10 @@ local plugin = {
     version = "1.*",
     event = "VeryLazy",
     config = require("plugconf.blink-cmp").setup,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      config = require("plugconf.nvim-lspconfig").setup,
+    },
   },
 
   {
