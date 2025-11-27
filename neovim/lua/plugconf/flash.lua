ec892f20ab59a47c238409c127772eec4dab09ae
neur1n <Jihang.Li@outlook.com>
2025-11-27 16:43:27 +0800

    chore(neovim): refine keymap for flash jump

--- neovim/lua/plugconf/flash.lua
+++ neovim/lua/plugconf/flash.lua
@@ -2,12 +2,15 @@ local M = {}
 
 function M.keymap()
   return {
-    {"s", "<Cmd>lua require('flash').jump({highlight = {backdrop = false}})<CR>", mode = {"n", "v"}, {noremap = true, silent = true}},
+    {"s", "<Cmd>lua require('flash').jump()<CR>", mode = {"n", "o", "x"}, {noremap = true, silent = true}},
   }
 end
 
 function M.setup()
   require("flash").setup({
+    highlight = {
+      backdrop = false,
+    },
     modes = {
       char = {
         highlight = {
