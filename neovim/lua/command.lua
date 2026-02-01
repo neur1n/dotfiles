5138ae62536ab22b61ff536eeb7bf937733fae7c
neur1n <Jihang.Li@outlook.com>
2026-02-01 12:42:49 +0800

    chore(neovim): format command.lua

--- neovim/lua/command.lua
+++ neovim/lua/command.lua
@@ -1,35 +1,50 @@
 local M = {}
 
 function M.setup()
-  vim.api.nvim_create_user_command("DeleteHiddenBuffer", function()
-    local buflist = vim.api.nvim_list_bufs()
+  vim.api.nvim_create_user_command("DeleteHiddenBuffer",
+    function()
+      local buflist = vim.api.nvim_list_bufs()
 
-    for _, bufnr in ipairs(buflist) do
-      if not vim.api.nvim_buf_get_option(bufnr, "buflisted") then
-        vim.api.nvim_buf_delete(bufnr, {force = false})
+      for _, bufnr in ipairs(buflist) do
+        if not vim.api.nvim_buf_get_option(bufnr, "buflisted") then
+          vim.api.nvim_buf_delete(bufnr, {force = false})
+        end
       end
-    end
-  end, {})
+    end,
+    {}
+  )
 
-  vim.api.nvim_create_user_command("TabToSpace", function()
-    vim.cmd("silent! execute '%s/\t/  /g'")
-  end, {})
+  vim.api.nvim_create_user_command("TabToSpace",
+    function()
+      vim.cmd("silent! execute '%s/\t/  /g'")
+    end,
+    {}
+  )
 
-  vim.api.nvim_create_user_command("ToggleSpell", function()
-    vim.wo.spell = not(vim.opt.spell:get())
-  end, {})
+  vim.api.nvim_create_user_command("ToggleSpell",
+    function()
+      vim.wo.spell = not(vim.opt.spell:get())
+    end,
+    {}
+  )
 
-  vim.api.nvim_create_user_command("ToggleStatusline", function()
-    if vim.o.laststatus == 2 then
-      vim.o.laststatus = 3
-    else
-      vim.o.laststatus = 2
-    end
-  end, {})
+  vim.api.nvim_create_user_command("ToggleStatusline",
+    function()
+      if vim.o.laststatus == 2 then
+        vim.o.laststatus = 3
+      else
+        vim.o.laststatus = 2
+      end
+    end,
+    {}
+  )
 
-  vim.api.nvim_create_user_command("TrimTrailingWhitespace", function()
-    vim.cmd("silent! execute '%s/\\s\\+$//g'")
-  end, {})
+  vim.api.nvim_create_user_command("TrimTrailingWhitespace",
+    function()
+      vim.cmd("silent! execute '%s/\\s\\+$//g'")
+    end,
+    {}
+  )
 end
 
 return M
