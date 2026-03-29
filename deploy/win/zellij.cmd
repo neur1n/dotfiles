dce81ce443777646a1f9f41a6634c10df56aaddc
neur1n <Jihang.Li@outlook.com>
2026-03-29 12:32:49 +0800

    fix(zellij): fix deployment script for Windows (natively supported since 0.44)

--- deploy/win/zellij.cmd
+++ deploy/win/zellij.cmd
@@ -1,7 +1,12 @@
 @echo off
 
 SET src=%~dp0.\..\..\zellij
-SET dst=%USERPROFILE%\.config\zellij
+SET dst=%APPDATA%\Zellij\config
+
+SET parent=%APPDATA%\Zellij
+IF NOT EXIST "%parent%" (
+  MKDIR %parent%
+)
 
 MKLINK /J %dst% %src%
 
