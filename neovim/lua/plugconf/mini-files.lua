85f2ed625918a1c995d5a00681d26591939c9c80
neur1n <Jihang.Li@outlook.com>
2026-06-12 10:57:58 +0800

    chore(neovim): make mini-files opaque

--- neovim/lua/plugconf/mini-files.lua
+++ neovim/lua/plugconf/mini-files.lua
@@ -8,6 +8,16 @@ end
 
 function M.setup()
   require("mini.files").setup()
+
+  local id = vim.api.nvim_create_augroup("n_mini", {clear = true})
+
+  vim.api.nvim_create_autocmd('User', {
+    group = id,
+    pattern = "MiniFilesWindowOpen",
+    callback = function(args)
+      vim.wo[args.data.win_id].winblend = 0
+    end,
+  })
 end
 
 return M
