50464e1846b4237e0ae324d3ad58684c58e8959c
neur1n <Jihang.Li@outlook.com>
2026-07-27 16:23:53 +0800

    chore(opencode): refine deployment script

--- deploy/win/opencode.cmd
+++ deploy/win/opencode.cmd
@@ -3,11 +3,6 @@
 SET src=%~dp0.\..\..\opencode
 SET dst=%USERPROFILE%\.config\opencode
 
-MKLINK /J %dst%\commands %src%\commands
-MKLINK /J %dst%\skills %src%\skills
-MKLINK /J %dst%\opencode-quota %src%\opencode-quota
-MKLINK %dst%\AGENTS.md %src%\AGENTS.md
-MKLINK %dst%\opencode.json %src%\opencode.json
-MKLINK %dst%\tui.json %src%\tui.json
+MKLINK /J %dst% %src%
 
 START %dst%
